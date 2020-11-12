f21f2041dd4084a6a5fde7cdebce49091f0ec4d3
David Salazar <dssalaza@gmail.com>
2020-11-11 22:55:12 -0500

    add checkout flow

--- checkout_test.py
+++ checkout_test.py
@@ -17,9 +17,23 @@ class LoginTest(unittest.TestCase):
         for x in range(1, max_swipes, 1):
             try:
                 if self.driver.find_element_by_ios_predicate('label == "Sauce Labs Onesie"'):
+                    item = self.driver.find_element_by_ios_predicate('label == "Sauce Labs Onesie"')
+                    item.click()
                     break
             except:
                 self.driver.execute_script("mobile: swipe", {'direction': 'up'})
 
-        item = self.driver.find_element_by_ios_predicate('label == "Sauce Labs Onesie"')
-        item.click()
+        for y in range(1, max_swipes, 1):
+            try:
+                if self.driver.find_element_by_accessibility_id('test-ADD TO CART'):
+                    add_to_cart_button = self.driver.find_element_by_accessibility_id('test-ADD TO CART')
+                    add_to_cart_button.click()
+                    break
+            except:
+                self.driver.execute_script("mobile: swipe", {'direction': 'up'})
+        
+        cart_button = self.driver.find_element_by_ios_class_chain('**/XCUIElementTypeOther[`name == "test-Cart"`]/XCUIElementTypeOther')
+        cart_button.click()
+
+        checkout_button = self.driver.find_element_by_accessibility_id('test-CHECKOUT')
+        checkout_button.click()
